a46f9002f97b7d713bf1ee362c4529c596c121ab
Wojciech Kucjas <wojciech.kucjas@gmail.com>
2021-08-10 21:49:59 +0200

    Changed url for index

--- iwl/iwl/urls.py
+++ iwl/iwl/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('iwish_list', include('iwish_list.urls')),
+    path('', include('iwish_list.urls')),
     path('admin/', admin.site.urls),
 ]
